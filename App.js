ca5669208e99dc8091f982799df9b591bff81d6f
github username <github email address>
2018-12-15 11:22:32 -0500

    add deleteing, opacity, and scroll touch features

--- App.js
+++ App.js
@@ -1,48 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
-import ListItem from './src/components/ListItem/ListItem'
-import List from './src/components/List/List'
-import UserInput from './src/components/UserInput/UserInput'
-
-
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   state = {
-    placeName: '',
     places: []
-  }
+  };
 
-  placeNameChangedHandler = (val) => {
-    this.setState({
-      placeName: val
-    })
-  }
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === '') {
-      return;
-    }
-
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
+      };
+    });
+  };
+
+  placeDeleteHander = (index) => {
+    this.setState(prevState => {
+      return{
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
       }
     })
   }
@@ -50,10 +30,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <UserInput value={this.state.placeName} inputHandler={this.placeNameChangedHandler} buttonHandler={this.placeSubmitHandler} />
-
-        <List places={this.state.places}/>
-
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} onItemDeleted={this.placeDeleteHander} />
       </View>
     );
   }
@@ -62,19 +40,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding:26,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
 });
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const listItem = (props) => (
-  <View style={styles.listItem}>
-    <Text>{props.placeName}</Text>
-  </View>
-)
+  <TouchableOpacity onPress={props.onItemPressed}>
+    <View style={styles.listItem}>
+        <Text>{props.placeName}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 const styles = StyleSheet.create({
     listItem: {
-      width: "100%",
-      padding: 10,
-      backgroundColor: "#eee",
-      margin: 5
+        width: "100%",
+        marginBottom: 5,
+        padding: 10,
+        backgroundColor: "#eee"
     }
-})
+});
 
-export default listItem
+export default listItem;
